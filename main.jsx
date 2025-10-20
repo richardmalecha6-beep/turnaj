bd94601975b924fa363c2bebba5bf9b0f81fd256
richardmalecha6-beep <richardmalecha6@gmail.com>
2025-10-20 16:49:23 +0200

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import DarkTournamentLanding from './DarkTournamentLanding.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <DarkTournamentLanding />
+  </React.StrictMode>,
+)
